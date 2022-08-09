752d667fc7b54ec8097d0394765baaf6f226f6b2
shannone <shannone426@gmail.com>
2022-08-08 20:32:28 -0400

    first class functions

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
+// function Monday() {
+//     exerciseRoutine(liftWeights);
+//     //console.log("go for a five mile run");
+//     //console.log("pump iron");
+// };
+// // function Tuesday() {
+// //     console.log("go for a five mile run");
+// //     console.log("swim 40 laps");
+// // };
+// // function Wednsday() {
+// //     console.log("go for a five mile run");
+// //     console.log("go for a five mile run");
+// // };
+// // function Thursday() {
+// //     console.log("go for a five mile run");
+// //     console.log("pump iron");
+// // };
+// // function Friday() {
+// //     console.log("go for a five mile run");
+// //     console.log("swim 40 laps");
+// // };
 
-function 
+// function runFiveMiles() {
+//     console.log("go for a five mile run");
+// };
+// function liftWeights() {
+//     console.log("pump iron")
+// };
+// function exerciseRoutine(postActivity) {
+//     runFiveMiles();
+//     postActivity();
+// };
+// console.log(Monday);
+
+
+function returnsANamedFunction() {};
